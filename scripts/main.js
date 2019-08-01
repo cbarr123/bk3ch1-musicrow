37b1b1d22d9da5773ed76de7706a195431caa8c1
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-08-01 13:15:09 -0500

    Got country working

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+const jumpStart = []
+const chatten = []
+const polar = []
+
+const createCountryBluegrass = (name, genre, age) => {
+    chatten.push( 
+    {
+        name: name,
+        genre: genre,
+        age: age
+    })
+    return
+}
+const createFunkRap = (name, genre, age) => {
+    jumpStart.push( 
+    {
+        name: name,
+        genre: genre,
+        age: age
+    })
+    return
+}
+const createPop = (name, genre, age) => {
+    polar.push( 
+    {
+        name: name,
+        genre: genre,
+        age: age
+    })
+    return
+}
+
+const atikens = createCountryBluegrass("Bruce Atikens", "Country", "23")
+const davis = createCountryBluegrass("Avilee Davis", "Country", "19")
+console.log(chatten)
+console.log(davis)
